1ac553164ce8833a2feca89a81e0432ed7358a6a
Hauzan Rafi Attallah <rafihauzan42@gmail.com>
2024-10-13 17:49:49 +0700

    Refactor Export component to remove PDF export option and fix date range picker

--- src/pages/Export.jsx
+++ src/pages/Export.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import Sidebar from "../components/Sidebar";
 import Topbar from "../components/Topbar";
-import { FaFileExcel, FaFilePdf, FaCalendarAlt } from "react-icons/fa";
+import { FaCalendarAlt } from "react-icons/fa";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { toast } from "react-toastify";
@@ -17,24 +17,16 @@ const Export = () => {
   const [endDate, setEndDate] = useState(null);
   const [exportType, setExportType] = useState("xlsx"); // Format default: Excel (.xlsx)
   const [showDatePicker, setShowDatePicker] = useState(false); // Untuk menampilkan date picker
+  const [showExportTypeDropdown, setShowExportTypeDropdown] = useState(false); // Untuk dropdown export type
   const datePickerRef = useRef(null); // Ref untuk date picker
+  const exportTypeRef = useRef(null); // Ref untuk dropdown export type
   const authToken = localStorage.getItem("token"); // Ambil token dari localStorage
 
-  // Fungsi untuk menangani pengiriman form
   const handleFormSubmit = async () => {
-    // Debugging tanggal
-    console.log("Start Date:", startDate);
-    console.log("End Date:", endDate);
-
-    // Gunakan format tanggal tanpa konversi UTC
     const formattedStartDate = format(startDate, "yyyy-MM-dd");
     const formattedEndDate = format(endDate, "yyyy-MM-dd");
 
-    console.log("Formatted Start Date:", formattedStartDate);
-    console.log("Formatted End Date:", formattedEndDate);
-
     if (!startDate || !endDate) {
-      console.error("Tanggal belum dipilih.");
       if (!toast.isActive("toast-error")) {
         toast.dismiss();
         toast.error("Harap pilih rentang tanggal!", {
@@ -47,12 +39,10 @@ const Export = () => {
     setLoading(true);
 
     try {
-      // Debugging request URL
-      console.log(`API Request URL: ${import.meta.env.VITE_API_URL}/export?type=${exportType}&startDate=${formattedStartDate}&endDate=${formattedEndDate}`);
-
-      // Mengirim permintaan untuk mengunduh file
       const response = await axios.get(
-        `${import.meta.env.VITE_API_URL}/export?type=${exportType}&startDate=${formattedStartDate}&endDate=${formattedEndDate}`,
+        `${
+          import.meta.env.VITE_API_URL
+        }/export?type=${exportType}&startDate=${formattedStartDate}&endDate=${formattedEndDate}`,
         {
           headers: {
             Authorization: `Bearer ${authToken}`,
@@ -62,16 +52,15 @@ const Export = () => {
         }
       );
 
-      // Pengecekan ukuran file untuk memastikan konten
-      console.log("Response size:", response.data.size);
-
-      // Membuat link untuk mengunduh file
       const url = window.URL.createObjectURL(new Blob([response.data]));
       const link = document.createElement("a");
       link.href = url;
       link.setAttribute(
         "download",
-        `export_${format(startDate, "yyyyMMdd")}_${format(endDate, "yyyyMMdd")}.${exportType}`
+        `export_${format(startDate, "yyyyMMdd")}_${format(
+          endDate,
+          "yyyyMMdd"
+        )}.${exportType}`
       );
       document.body.appendChild(link);
       link.click();
@@ -84,7 +73,6 @@ const Export = () => {
     } catch (error) {
       if (!toast.isActive("toast-error")) {
         toast.dismiss();
-        console.error("Error exporting data:", error);
         toast.error("Gagal mengekspor data, silakan coba lagi.", {
           toastId: "toast-error",
         });
@@ -93,12 +81,6 @@ const Export = () => {
     }
   };
 
-  // Fungsi untuk menampilkan nama file yang dipotong jika terlalu panjang
-  const getDisplayFileName = () => {
-    return exportType === "xlsx" ? "Excel (.Xlsx)" : "PDF (.Pdf)";
-  };
-
-  // Fungsi untuk menampilkan date range dalam format yang singkat
   const getDisplayDateRange = (start, end) => {
     if (start && end) {
       return `${format(start, "dd MMM", { locale: id })} - ${format(
@@ -110,14 +92,20 @@ const Export = () => {
     return "Pilih Tanggal";
   };
 
-  // Event listener untuk menutup DatePicker saat klik di luar
   useEffect(() => {
     const handleClickOutside = (event) => {
       if (
         datePickerRef.current &&
         !datePickerRef.current.contains(event.target)
       ) {
-        setShowDatePicker(false); // Sembunyikan DatePicker
+        setShowDatePicker(false);
+      }
+
+      if (
+        exportTypeRef.current &&
+        !exportTypeRef.current.contains(event.target)
+      ) {
+        setShowExportTypeDropdown(false);
       }
     };
 
@@ -125,7 +113,7 @@ const Export = () => {
     return () => {
       document.removeEventListener("mousedown", handleClickOutside);
     };
-  }, [datePickerRef]);
+  }, [datePickerRef, exportTypeRef]);
 
   return (
     <>
@@ -140,44 +128,47 @@ const Export = () => {
           {/* Container untuk Export Options */}
           <div className="p-6 bg-white rounded-lg shadow-lg md:p-8">
             <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 relative">
-              {/* Pilihan Format Export */}
-              <div className="flex items-center justify-between p-4 border rounded-lg cursor-pointer md:p-6">
+              {/* Dropdown untuk pilihan Export Type */}
+              <div
+                className="relative flex items-center justify-between p-4 border rounded-lg cursor-pointer md:p-6"
+                onClick={() =>
+                  setShowExportTypeDropdown(!showExportTypeDropdown)
+                }
+              >
                 <div className="flex items-center">
-                  {exportType === "xlsx" ? (
-                    <FaFileExcel className="mr-4 text-2xl text-gray-500" />
-                  ) : (
-                    <FaFilePdf className="mr-4 text-2xl text-gray-500" />
-                  )}
                   <div className="truncate max-w-[200px]">
                     <p className="text-gray-500">Export As</p>
                     <h2 className="text-lg font-bold md:text-xl">
-                      {getDisplayFileName()}
+                      {exportType === "xlsx" ? "Excel (.Xlsx)" : "PDF (.Pdf)"}
                     </h2>
                   </div>
                 </div>
-                <div className="flex items-center space-x-4">
-                  <button
-                    className={`${
-                      exportType === "xlsx"
-                        ? "bg-green-500 text-white"
-                        : "bg-gray-200 text-gray-500"
-                    } px-4 py-2 rounded-md`}
-                    onClick={() => setExportType("xlsx")}
-                  >
-                    Excel
-                  </button>
-                  <button
-                    className={`${
-                      exportType === "pdf"
-                        ? "bg-red-500 text-white"
-                        : "bg-gray-200 text-gray-500"
-                    } px-4 py-2 rounded-md`}
-                    onClick={() => setExportType("pdf")}
-                  >
-                    PDF
-                  </button>
-                </div>
+                <div className="text-4xl md:text-6xl">&rsaquo;</div>
               </div>
+
+              {showExportTypeDropdown && (
+                <div
+                  ref={exportTypeRef}
+                  className="absolute z-50 mt-32 bg-white border rounded-lg shadow-lg p-4 w-72 right-0 md:mr-64 md:left-0"
+                >
+                  <div>
+                    <label className="block text-sm font-semibold mb-1">
+                      Select Export Type
+                    </label>
+                    <select
+                      value={exportType}
+                      onChange={(e) => {
+                        setExportType(e.target.value);
+                        setShowExportTypeDropdown(false); // Tutup dropdown setelah tipe dipilih
+                      }}
+                      className="block w-full p-2 mt-1 border border-gray-300 rounded-lg shadow-sm"
+                    >
+                      <option value="xlsx">Excel (.Xlsx)</option>
+                      <option value="pdf">PDF (.Pdf)</option>
+                    </select>
+                  </div>
+                </div>
+              )}
 
               {/* Date Range Picker */}
               <div
@@ -196,10 +187,9 @@ const Export = () => {
                 <div className="text-4xl md:text-6xl">&rsaquo;</div>
               </div>
 
-              {/* DatePicker muncul tanpa auto close */}
               {showDatePicker && (
                 <div
-                  ref={datePickerRef} // Tambahkan ref ke div yang mengandung DatePicker
+                  ref={datePickerRef}
                   className="absolute z-50 mt-32 bg-white border rounded-lg shadow-lg p-4 w-72 right-0 md:mr-64 md:right-0"
                 >
                   <div>
@@ -209,7 +199,6 @@ const Export = () => {
                     <DatePicker
                       selected={startDate}
                       onChange={(date) => {
-                        console.log("Selected Start Date:", date); // Debug tanggal
                         setStartDate(date);
                       }}
                       className="block w-full p-2 mt-1 border border-gray-300 rounded-lg shadow-sm"
@@ -223,8 +212,8 @@ const Export = () => {
                     <DatePicker
                       selected={endDate}
                       onChange={(date) => {
-                        console.log("Selected End Date:", date); // Debug tanggal
                         setEndDate(date);
+                        setShowDatePicker(false);
                       }}
                       className="block w-full p-2 mt-1 border border-gray-300 rounded-lg shadow-sm"
                       placeholderText="Pilih Tanggal Akhir"
@@ -238,7 +227,7 @@ const Export = () => {
                 <button
                   className="bg-[#B4252A] hover:bg-[#8E1F22] text-white font-bold py-2 md:py-3 px-8 md:px-16 rounded-md mt-6 md:mt-9"
                   onClick={handleFormSubmit}
-                  disabled={loading} // Nonaktifkan tombol saat sedang loading
+                  disabled={loading}
                 >
                   {loading ? (
                     <ReactLoading
